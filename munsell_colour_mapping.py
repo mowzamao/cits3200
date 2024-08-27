d613e33663e9b719a18287eb6bb0a2530baf52e0
mjb44552 <m50booth@gmail.com>
2024-08-27 18:04:03 +0800

    rearranged order of functions in script to improve readability

--- munsell_colour_mapping.py
+++ munsell_colour_mapping.py
@@ -6,14 +6,6 @@ def get_munsell_dictionary(file_path_list:list,required_columns:list):
     munsell_dict = create_munsell_dictionary(munsell_df)
     return munsell_dict
 
-def create_munsell_dictionary(munsell_df:pd.DataFrame):
-    munsell_dict = {}
-    for key,group in munsell_df.groupby('munsell'):
-        lab  = group[['l*','a*','b*']].values.tolist()[0]
-        rgb = group[['r','g','b']].values.tolist()[0]
-        munsell_dict[key] = {'rgb':rgb,'lab':lab}
-    return munsell_dict
-
 def create_munsell_dataframe(file_path_list:list,required_columns:list):
     munsell_df = pd.DataFrame()
     for file_path in file_path_list:
@@ -22,6 +14,32 @@ def create_munsell_dataframe(file_path_list:list,required_columns:list):
         df = create_munsell_value_column(df)
         munsell_df = join_dataframes(munsell_df,df)
     return munsell_df
+
+def create_munsell_dictionary(munsell_df:pd.DataFrame):
+    munsell_dict = {}
+    for key,group in munsell_df.groupby('munsell'):
+        lab  = group[['l*','a*','b*']].values.tolist()[0]
+        rgb = group[['r','g','b']].values.tolist()[0]
+        munsell_dict[key] = {'rgb':rgb,'lab':lab}
+    return munsell_dict
+
+def read_munsell_csv_data(file_path:str):
+    try:
+        df = pd.read_csv(file_path)
+        return df
+    except FileNotFoundError:
+        print('File Not Found Error: check file path')
+        return None
+    
+def df_column_filtering(df:pd.DataFrame,required_columns:list):
+    df.columns = [column.strip().lower() for column in df.columns]
+    column_subset = [column for column in df.columns if column in required_columns]
+    return df[column_subset]
+
+def create_munsell_value_column(df:pd.DataFrame):
+    df['munsell'] = df['h'].astype(str) + ' ' + df['v'].astype(str) + ' ' + df['c'].astype(str)
+    df = df.drop(columns = ['h','v','c'])
+    return df
 
 def join_dataframes(munsell_df:pd.DataFrame,df:pd.DataFrame):
     if munsell_df.empty:
@@ -33,25 +51,6 @@ def join_dataframes(munsell_df:pd.DataFrame,df:pd.DataFrame):
     else:
         print("Error: join cannot occur,column names don't match")
         return None
-
-def create_munsell_value_column(df:pd.DataFrame):
-    df['munsell'] = df['h'].astype(str) + ' ' + df['v'].astype(str) + ' ' + df['c'].astype(str)
-    df = df.drop(columns = ['h','v','c'])
-    return df
-
-def read_munsell_csv_data(file_path:str):
-    try:
-        df = pd.read_csv(file_path)
-        return df
-    except FileNotFoundError:
-        print('File Not Found Error: check file path')
-        return None
-
-def df_column_filtering(df:pd.DataFrame,required_columns:list):
-    df.columns = [column.strip().lower() for column in df.columns]
-    column_subset = [column for column in df.columns if column in required_columns]
-    return df[column_subset]
-    
 
 munsell_dict = get_munsell_dictionary(['munsell_data/real_CIELAB.csv','munsell_data/real_sRGB.csv'],['l*','a*','b*','h','v','c','r','g','b'])
 print(munsell_dict['10RP 1 2'])
