c3c66efdbc89dbe79610de94ff9d7d23dda3de11
CustomEpicness1 <23154502@student.uwa.edu.au>
2024-08-30 23:32:13 +0800

    Notion ID 3 - Corrected incorrect terminology in image to array functions

--- image_processing/image_formatting.py
+++ image_processing/image_formatting.py
@@ -2,14 +2,14 @@ import cv2 as cv
 import numpy as np
 
 def img_rgb_array(image, is_BGR=False):
-    """Turns an OpenCV RGB or BGR image into 3 seperate Red, Green, and Blue arrays of all the same length"""
+    """Turns an OpenCV RGB or BGR image into 3 seperate Red, Green, and Blue arrays of all the same shape"""
     red = image[:,:,2] if is_BGR else image[:,:,0]
     green = image[:,:,1]
     blue = image[:,:,0] if is_BGR else image[:,:,2]
     return red, green, blue
 
 def img_lab_array(image):
-    """Turns an OpenCV CIELAB image into 3 seperate L*, a*, and b* arrays of all the same length"""
+    """Turns an OpenCV CIELAB image into 3 seperate L*, a*, and b* arrays of all the same shape"""
     light = image[:,:,0]
     a_coord = image[:,:,1]
     b_coord = image[:,:,2]
