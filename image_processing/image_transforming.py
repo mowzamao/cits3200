6391a05b302a8088c803db9388775fb4a06c1cfd
Sophie Mowe <87960309+mowzamao@users.noreply.github.com>
2024-09-04 16:53:25 +0800

    ID2: Added documentation for get_sediment_core function

--- image_processing/image_transforming.py
+++ image_processing/image_transforming.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import argparse
+
 
 def import_img(image_path):
     """ Imports an image from a file path"""
@@ -8,7 +8,25 @@ def import_img(image_path):
     assert image is not None, "No image found at the specified path"
     return image
 
-def get_sediment_cores(image):
+def get_sediment_cores(filepath):
+    """
+    Basic function to detect coloured sediment cores in an image. Very
+    specific to the images we have been provided: for instance, removes white
+    white and grey areas of the image to make processing easier.
+    Draws bounding boxes around the sediment cores and displays the image with the 
+    bounding boxes. Prints the locations of the sediment cores.
+
+    Params
+    ------
+        filepath (str): path to the image file
+
+    Returns
+    -------
+        array of arrays of the form [x, y, w, h] where x, y are the coordinates of the 
+        top left corner of the bounding box
+    """
+    image = import_img(filepath)
+    
     # remove grey and white areas of the image
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     lower_grey = np.array([0, 0, 50])
@@ -31,7 +49,6 @@ def get_sediment_cores(image):
     sediment_cores = []
     for contour in contours:
         area = cv2.contourArea(contour)
-        
         if area > 100000:  
             x, y, w, h = cv2.boundingRect(contour)
             if w > h*2 or h > w*2:
@@ -48,53 +65,53 @@ def get_sediment_cores(image):
     cv2.destroyAllWindows()
     return sediment_cores
 
-image = import_img('image_processing\image-data\MI-24_03\SCREEN banner 96dpi-3148-2.jpg')
-# sediment_cores = get_sediment_cores(image)
+sediment_cores = get_sediment_cores('image_processing\image-data\MI-24_04\SCREEN banner 96dpi-3293.jpg')
 
-def select_cm(image):
-    def shape_selection(event, x, y, flags, param): 
-        # grab references to the global variables 
-        global ref_point, crop 
+# def select_cm(image):
+"""" Selects a region of interest in an image: Use for cropping the image to a cm? """
+#     def shape_selection(event, x, y, flags, param): 
+#         # grab references to the global variables 
+#         global ref_point, crop 
     
-        # if the left mouse button was clicked, record the starting 
-        # (x, y) coordinates and indicate that cropping is being performed 
-        if event == cv2.EVENT_LBUTTONDOWN: 
-            ref_point = [(x, y)] 
+#         # if the left mouse button was clicked, record the starting 
+#         # (x, y) coordinates and indicate that cropping is being performed 
+#         if event == cv2.EVENT_LBUTTONDOWN: 
+#             ref_point = [(x, y)] 
     
-        # check to see if the left mouse button was released 
-        elif event == cv2.EVENT_LBUTTONUP: 
-            # record the ending (x, y) coordinates and indicate that 
-            # the cropping operation is finished 
-            ref_point.append((x, y)) 
+#         # check to see if the left mouse button was released 
+#         elif event == cv2.EVENT_LBUTTONUP: 
+#             # record the ending (x, y) coordinates and indicate that 
+#             # the cropping operation is finished 
+#             ref_point.append((x, y)) 
     
-            # draw a rectangle around the region of interest 
-            cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2) 
-            cv2.imshow("image", image) 
+#             # draw a rectangle around the region of interest 
+#             cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2) 
+#             cv2.imshow("image", image) 
     
-    # load the image, clone it, and setup the mouse callback function 
-    clone = image.copy() 
-    cv2.namedWindow("image") 
-    cv2.setMouseCallback("image", shape_selection) 
+#     # load the image, clone it, and setup the mouse callback function 
+#     clone = image.copy() 
+#     cv2.namedWindow("image") 
+#     cv2.setMouseCallback("image", shape_selection) 
     
     
-    # keep looping until the 'q' key is pressed 
-    while True: 
-        # display the image and wait for a keypress 
-        cv2.imshow("image", image) 
-        key = cv2.waitKey(1) & 0xFF
+#     # keep looping until the 'q' key is pressed 
+#     while True: 
+#         # display the image and wait for a keypress 
+#         cv2.imshow("image", image) 
+#         key = cv2.waitKey(1) & 0xFF
     
-        # press 'r' to reset the window 
-        if key == ord("r"): 
-            image = clone.copy() 
+#         # press 'r' to reset the window 
+#         if key == ord("r"): 
+#             image = clone.copy() 
     
-        # if the 'c' key is pressed, break from the loop 
-        elif key == ord("c"): 
-            break
+#         # if the 'c' key is pressed, break from the loop 
+#         elif key == ord("c"): 
+#             break
     
-    if len(ref_point) == 2: 
-        crop_img = clone[ref_point[0][1]:ref_point[1][1], ref_point[0][0]: 
-                                                            ref_point[1][0]] 
-        cv2.imshow("crop_img", crop_img) 
-        cv2.waitKey(0) 
+#     if len(ref_point) == 2: 
+#         crop_img = clone[ref_point[0][1]:ref_point[1][1], ref_point[0][0]: 
+#                                                             ref_point[1][0]] 
+#         cv2.imshow("crop_img", crop_img) 
+#         cv2.waitKey(0) 
 
-select_cm(image)
+# select_cm(image)
