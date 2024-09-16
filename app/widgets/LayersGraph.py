9acef9e4ec468933f6809e7cd478a98d399e0baf
mjb44552 <m50booth@gmail.com>
2024-09-16 15:02:45 +0800

    added docstrings to LayersGraph

--- app/widgets/LayersGraph.py
+++ app/widgets/LayersGraph.py
@@ -10,19 +10,28 @@ class LayersGraph(FigureCanvasQTAgg):
     """A wrapper class for a Matplotlib plot of the sediment layers"""
 
     def __init__(self, parent=None, width=5, height=5, dpi=100, df = None):
+
+        #defining metadate for layers plot
         self.layers_title_min_fontsize = 3
         self.layers_title_max_fontsize = 10 
         self.layers_title_base_fontsize = 7 
         self.layers_min_width = 0.15    
-
-        #define metadata for layer plot
         height = len(df)
         width = max(1, height//10)
-        core_as_grid = np.zeros((height, width, 3), dtype=int)
 
-        #populate the numpy array with RGB values which numpy.imshow will render
-        core_as_grid = self.populateCore_as_grid(df,height, width,core_as_grid)
+        core_as_grid = self.createCore_as_grid(df,height, width)
+        self.setupLayersFigure(width,height,dpi)
+        super(LayersGraph, self).__init__(self.layers_fig)
+        self.renderLayersFigure(core_as_grid)
 
+
+    def renderLayersFigure(self,core_as_grid):
+        #render image
+        self.layers_axes.imshow(core_as_grid, aspect='auto')
+        #show title
+        self.layers_axes.set_title("Colour Layers",fontsize = 8, fontweight='bold')
+
+    def setupLayersFigure(self,width,height,dpi):
         #define the figure the plot will be rendered in 
         self.layers_fig = Figure(figsize=(width, height), dpi=dpi)
         self.layers_axes = self.layers_fig.add_subplot(111)
@@ -31,15 +40,8 @@ class LayersGraph(FigureCanvasQTAgg):
         self.layers_axes.get_xaxis().set_ticks([])
         self.layers_axes.get_yaxis().set_ticks([])
 
-        #initialise instance of the plot and render image 
-        super(LayersGraph, self).__init__(self.layers_fig)
-        self.layers_axes.imshow(core_as_grid, aspect='auto')
 
-        #show title
-        self.layers_axes.set_title("Colour Layers",fontsize = 8, fontweight='bold')
-
-
-    def populateCore_as_grid(self,df:pd.DataFrame,height:int,width:int,core_as_grid:np.ndarray):
+    def createCore_as_grid(self,df:pd.DataFrame,height:int,width:int):
         """
         Function to add RGB data to the core_as_grid variable for the Layers plot. 
         The array has every row as a specific layer / colour and each column shows 
@@ -48,6 +50,8 @@ class LayersGraph(FigureCanvasQTAgg):
         Parameters:
             df(pd.Dataframe): the data to be rendered in the layers plot.
         """
+        core_as_grid = np.zeros((height, width, 3), dtype=int)
+
         for row in range(height):
             for col in range(width):
                 for channel in range(3):
@@ -55,47 +59,61 @@ class LayersGraph(FigureCanvasQTAgg):
         return core_as_grid
     
     def resizeEvent(self, event):
-        #set tight_layout setting to figure if figure has dimensions
-        if self.verifyTightLayout():
+        """
+        Function which resizes graphical parameters of the LayersGraph 
+        as PyQt window is changed.  
+        """
+        if self.verifyDimensions():
             self.layers_fig.tight_layout()
 
-        #change font size if current font size and subplot width not below minimum values
-        if self.verifyLabelFontChange():
+        if self.verifyTitleFont():
             self.setFontSize()
         
-        # Redraw the figure - inherited from FigureCanvasQTAgg
         self.draw()
         # Initialise an instance of the new figure  
         super().resizeEvent(event)
 
-    def verifyLabelFontChange(self):
-        if self.verifytitleGreaterThanMinFontSize() is False:
+    def verifyTitleFont(self):
+        if self.verifytitleGreaterThanMinFont() is False:
             return None
-        if self.verifySubplotGreaterThanMinWidth() is False:
+        if self.verifyFigGreaterThanMinWidth() is False:
             return None
         return True
     
-    def verifytitleGreaterThanMinFontSize(self):
+    def verifytitleGreaterThanMinFont(self):
+        """
+        Function verifying if LayerGraph's newfont is greater than it's
+        minimum allowed font. 
+        """
         new_fontsize = self.calcNewFont()
         if new_fontsize <= self.layers_title_min_fontsize:
             return False
         return True
     
-    def verifySubplotGreaterThanMinWidth(self):
+    def verifyFigGreaterThanMinWidth(self):
+        """
+        Function which checks LayersGraph Figure is less than minimum width. 
+        """
         #calculation of subplot size in inches, requires conversion form normalised units
         norm_width = self.layers_axes.get_position().width
         fig_width, fig_height = self.layers_fig.get_size_inches()
         fig_width_inches = fig_width *norm_width
+        
         if fig_width_inches <= self.layers_min_width:
             return False
         return True
 
     def setFontSize(self):
+        """
+        Function which resets fontsize of LayersGraph Title
+        """
         new_fontsize = self.calcNewFont()
         self.layers_axes.set_title("Colour Layers",fontsize = new_fontsize, fontweight='bold')
 
-
     def calcNewFont(self):
+        """
+        Function which calculates new fontsize for LayersGraph as it's resized. 
+        """
         width, height = self.layers_fig.get_size_inches()
         scale_factor = width / 0.9
 
@@ -106,10 +124,11 @@ class LayersGraph(FigureCanvasQTAgg):
         font_size = max(self.layers_title_min_fontsize,new_fontsize)
         return font_size
 
-    def verifyTightLayout(self):
-        # Get the current width and height of the figure
+    def verifyDimensions(self):
+        """
+        Function to check dimensions of layers Graph is greater than 0.
+        """
         width, height = self.layers_fig.get_size_inches()
-
         if width > 0 and height > 0:
             return True
         else:
