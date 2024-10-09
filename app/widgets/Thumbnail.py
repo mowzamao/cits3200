0745c08868025ac119bcf2c3cba94fc5da6f648b
Rishi3524 <168170143+Rishi3524@users.noreply.github.com>
2024-10-09 13:53:39 +0800

    Update Thumbnail.py

--- app/widgets/Thumbnail.py
+++ app/widgets/Thumbnail.py
@@ -1,16 +1,17 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QFrame
-from PyQt6.QtGui import QPixmap, QFont, QFontMetrics
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QFrame, QDialog, QPushButton, QVBoxLayout
+from PyQt6.QtGui import QPixmap, QFontMetrics
 from PyQt6.QtCore import Qt
 
 class Thumbnail(QFrame):
-    def __init__(self, image_path = None):
+    def __init__(self, image_path=None, main_window=None):
         super().__init__()
-    
+
         self.image_path = image_path
+        self.main_window = main_window  # Reference to the main window to send panel updates
         self.is_left = False
         self.is_right = False
-        
+
         # Set up the layout
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
@@ -18,18 +19,17 @@ class Thumbnail(QFrame):
         self.image = QLabel(self)
         self.caption = QLabel(self)
 
-        self.image.setAlignment(Qt.AlignmentFlag.AlignCenter)    
+        self.image.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.caption.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.layout.addWidget(self.image, stretch = 5)
-        self.layout.addWidget(self.caption, stretch = 1)
+        self.layout.addWidget(self.image, stretch=5)
+        self.layout.addWidget(self.caption, stretch=1)
 
-
-        #Setting up the caption
-        self.caption.setWordWrap(False)  # Disable word wrapping
+        # Setting up the caption
+        self.caption.setWordWrap(False)
         self.caption.setStyleSheet("color: black; font-weight: bold; padding: 5px;")
 
-        # Converting the image_path to the image name for the caption
+        # Caption setup
         try:
             name = image_path.split("/")[-1]
             try:
@@ -39,44 +39,62 @@ class Thumbnail(QFrame):
         except:
             name = image_path
 
-        # Ensuring caption text doesn't flow out of bounds
         font_metrics = QFontMetrics(self.caption.font())
-        elided_name = font_metrics.elidedText(name, Qt.TextElideMode.ElideLeft, self.image.width()) 
+        elided_name = font_metrics.elidedText(name, Qt.TextElideMode.ElideLeft, self.image.width())
         self.caption.setText(elided_name)
 
-
-        # Setting the style
+        # Set the style
         self.setStyleSheet("""
         QFrame {
             background-color: rgba(255, 255, 255, 150);
             border-radius: 0px;
         }
         QFrame:hover {
-            background-color: lightgray; 
+            background-color: lightgray;
         }
         QLabel {
             font-size: 14px;
             color: black;
         }
-         """)
+        """)
 
-        # Loading the image
+        # Load the image
         self.load_image()
-        
 
     def load_image(self):
-        # Loading the image and resizing it to thumbnail size
         pixmap = QPixmap(self.image_path)
-        self.image.resize(100, 200)     
+        self.image.resize(100, 200)
         self.image.setPixmap(pixmap.scaled(self.image.size(), Qt.AspectRatioMode.KeepAspectRatio))
 
-
     def mousePressEvent(self, event):
-        # Check if the mouse click is within the widget
+        # Open the dialog when thumbnail is clicked
         if event.button() == Qt.MouseButton.LeftButton:
-            if not self.is_left:
-                self.setStyleSheet("""QFrame {background-color: lightblue;}""")
-                self.is_left = True
-            else:
-                self.setStyleSheet("""QFrame {background-color: rgba(255, 255, 255, 150);}""")
-                self.is_left = False
+            self.show_panel_selection_dialog()
+
+    def show_panel_selection_dialog(self):
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Select Panel")
+        layout = QVBoxLayout()
+
+        label = QLabel("Select a panel to display the graph:")
+        layout.addWidget(label)
+
+        left_button = QPushButton("Left Panel")
+        right_button = QPushButton("Right Panel")
+
+        layout.addWidget(left_button)
+        layout.addWidget(right_button)
+
+        left_button.clicked.connect(lambda: self.select_panel("left", dialog))
+        right_button.clicked.connect(lambda: self.select_panel("right", dialog))
+
+        dialog.setLayout(layout)
+        dialog.exec()
+
+    def select_panel(self, panel_side, dialog):
+        if self.main_window:
+            # Notify main window to update the corresponding panel
+            self.main_window.update_graph_panel(self.image_path, panel_side)
+    
+        dialog.accept()  # Close the dialog
+
