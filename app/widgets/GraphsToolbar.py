7506648770e65afee4ba7922df07859f75a3b3ab
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-10-11 16:39:04 +0800

    Adding icon for units button

--- app/widgets/GraphsToolbar.py
+++ app/widgets/GraphsToolbar.py
@@ -22,7 +22,7 @@ class GraphsToolbar(NavigationToolbar):
 
         self.add_grid_button()
         self.add_save_buttons()
-        self.add_unit_button()
+        self.add_units_button()
         self.remove_buttons(["Subplots", "Customize", "Save"])
         self.replace_icons()
 
@@ -83,6 +83,13 @@ class GraphsToolbar(NavigationToolbar):
         self.grid_action = QAction("Toggle Grid", self, icon = self.svg_to_icon(grid_svg))
         self.insertAction(self.actions()[6], self.grid_action)
         self.grid_action.triggered.connect(self.toggle_grid)
+
+    def add_units_button(self):
+        units_svg = """<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><rect width="100%" height="100%" fill="#f3f2f0" /> <path d="M21 6H3c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 10H3V8h2v4h2V8h2v4h2V8h2v4h2V8h2v4h2V8h2v8z"/></svg>"""
+        self.units_action = QAction("Change Units", self, icon = self.svg_to_icon(units_svg))
+        self.insertAction(self.actions()[6], self.units_action)
+        self.units_action.triggered.connect(self.toggle_units)
+
 
     def add_save_buttons(self):
         csv_svg = """<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-filetype-csv" viewBox="0 0 16 16"><rect width="100%" height="100%" fill="#f3f2f0" /> <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM3.517 14.841a1.13 1.13 0 0 0 .401.823q.195.162.478.252.284.091.665.091.507 0 .859-.158.354-.158.539-.44.187-.284.187-.656 0-.336-.134-.56a1 1 0 0 0-.375-.357 2 2 0 0 0-.566-.21l-.621-.144a1 1 0 0 1-.404-.176.37.37 0 0 1-.144-.299q0-.234.185-.384.188-.152.512-.152.214 0 .37.068a.6.6 0 0 1 .246.181.56.56 0 0 1 .12.258h.75a1.1 1.1 0 0 0-.2-.566 1.2 1.2 0 0 0-.5-.41 1.8 1.8 0 0 0-.78-.152q-.439 0-.776.15-.337.149-.527.421-.19.273-.19.639 0 .302.122.524.124.223.352.367.228.143.539.213l.618.144q.31.073.463.193a.39.39 0 0 1 .152.326.5.5 0 0 1-.085.29.56.56 0 0 1-.255.193q-.167.07-.413.07-.175 0-.32-.04a.8.8 0 0 1-.248-.115.58.58 0 0 1-.255-.384zM.806 13.693q0-.373.102-.633a.87.87 0 0 1 .302-.399.8.8 0 0 1 .475-.137q.225 0 .398.097a.7.7 0 0 1 .272.26.85.85 0 0 1 .12.381h.765v-.072a1.33 1.33 0 0 0-.466-.964 1.4 1.4 0 0 0-.489-.272 1.8 1.8 0 0 0-.606-.097q-.534 0-.911.223-.375.222-.572.632-.195.41-.196.979v.498q0 .568.193.976.197.407.572.626.375.217.914.217.439 0 .785-.164t.55-.454a1.27 1.27 0 0 0 .226-.674v-.076h-.764a.8.8 0 0 1-.118.363.7.7 0 0 1-.272.25.9.9 0 0 1-.401.087.85.85 0 0 1-.478-.132.83.83 0 0 1-.299-.392 1.7 1.7 0 0 1-.102-.627zm8.239 2.238h-.953l-1.338-3.999h.917l.896 3.138h.038l.888-3.138h.879z"/></svg>"""
@@ -187,16 +194,6 @@ class GraphsToolbar(NavigationToolbar):
             if df is not None:
                 df.to_excel(file_name, index=False)
 
-    def add_unit_button(self):
-        
-        icon = QIcon.fromTheme(f"{getcwd()}/app/style/grid.svg")
-
-        self.unit_action = QAction('Change Units',self,icon = icon)
-
-        self.insertAction(self.actions()[6],self.unit_action)
-
-
-        self.unit_action.triggered.connect(self.toggle_units)
     
     def toggle_units(self):
         self.parent.graphs.colours_graph.units = self.getNewUnit(self.parent.graphs.colours_graph.units)
